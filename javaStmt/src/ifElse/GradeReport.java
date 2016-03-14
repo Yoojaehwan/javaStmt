40d0423f50d4acb523b44d42a6bb98a74334df00
rjh1226 <rjh1226@naver.com>
2016-03-14 17:53:32 +0900

    dd

--- javaStmt/src/ifElse/GradeReport.java
+++ javaStmt/src/ifElse/GradeReport.java
@@ -3,4 +3,30 @@ package ifElse;
 public class GradeReport {
 	String name, subject, grade;
 	int score ;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getGrade() {
+		return grade;
+	}
+	public void setGrade(String grade) {
+		this.grade = grade;
+	}
+	public int getScore() {
+		return score;
+	}
+	public void setScore(int score) {
+		this.score = score;
+	}
+	
+	
 }
